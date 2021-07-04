575e83b0e8d692de647b6a3468b15ff1711ce75b
nobasksio <a420673@yandex.ru>
2021-07-04 19:17:49 +0800

    первая

--- 1/index.js
+++ 1/index.js
@@ -1,1 +1,24 @@
-console.log('1')
+
+
+function getResult(data) {
+  let canFinish = false;
+  while(!canFinish) {
+    canFinish = true;
+    for(let r = 1; r < data.length; r += 1) {
+      const lItem = data[r - 1];
+      const rItem = data[r];
+      if (lItem > rItem) {
+        data[r - 1] = rItem;
+        data[r] = lItem;
+        canFinish = false;
+      }
+    }
+  }
+
+
+  return data;
+}
+
+
+const data = [0, 1, 3,3, 4];
+console.log(getResult(data));
