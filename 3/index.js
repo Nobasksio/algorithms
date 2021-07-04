980b8e06d9a49e6b512719416de4cd37330bba82
nobasksio <a420673@yandex.ru>
2021-07-04 20:42:15 +0800

    третья

--- 3/index.js
+++ 3/index.js
@@ -3,23 +3,25 @@ function getResult(data) {
 
   for(let l = data.length - 2; l >= 0; l -= 1) {
 
-   for(let r = l + 1; r < data.length; r += 1){
+    const item = data[l];
+    let indexMemo = l;
 
-     if (data[r - 1] > data[r]) {
-       const temp = data[r];
-       data[r] = data[r - 1];
-       data[r - 1] = temp
-     } else {
-       break
+     for(let r = l + 1; r < data.length; r += 1){
+       if (item > data[r]) {
+         indexMemo = r;
+         const temp = data[r];
+         data[r - 1] = temp;
+       } else {
+         break
+       }
      }
 
-   }
-
+   data[indexMemo] = item;
   }
 
   return data;
 }
 
 
-const data = [5, 0, 123, 23,1, 123123,];
+const data = [5, 1, 213, 11,0, 123, 23,1, 123123, 2133];
 console.log(getResult(data));
